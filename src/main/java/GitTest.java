2529adb1a64a6c242cbd0e704653d752be679a98
zbx <2289934894@qq.com>
2022-09-19 23:39:20 +0800

    master commit1

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
+        System.out.println("master test");
     }
 }
